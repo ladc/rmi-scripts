abf75f02e6bdd881674cea49cbfd56ea73d9ad8c
Lesley De Cruz <lesley.decruz@meteo.be>
2025-03-20 23:22:16 +0000

    Optimize jobscript to launch steps on the HPC.
    Add scripts for plotting obs and nowcast (including a GIF).

--- jobscript_plot.sh
+++ jobscript_plot.sh
@@ -4,7 +4,7 @@
 #PBS -l select=1:ncpus=8:ompthreads=8:mem=24GB
 #PBS -j oe
 #PBS -W umask=022
-#PBS -N pysteps_run
+#PBS -N pysteps_plot
 
 # Run this script as follows:
 # qsub -vtimestamp=202308251600 jobscript_plot.sh 
@@ -19,14 +19,21 @@ echo -----------------------------------
 source /home/ledecruz/.bashrc
 export OMP_NUM_THREADS=8
 workdir=/home/ledecruz/projects/rmi-scripts
-script=run_steps.py
-#script=plot_nwc.py
+logdir=${workdir}/data/log
+logfile=${logdir}/plot_${timestamp}
+script=plot_nwc.py
 conda init
 conda activate pysteps_dev
 
 set -x
 cd $workdir
-python ${script} $timestamp 72 8 8 > $workdir/data/log/steps_$timestamp.log 2> $workdir/data/log/steps_$timestamp.err
+python ${script} $timestamp > ${logfile}.log 2> ${logfile}.err
+set +x
+
+module load ImageMagick
+
+set -x
+./generate_grid_gif.sh $timestamp >> ${logfile}.log 2>> ${logfile}.err
 set +x
 
 echo -----------------------------------
--- jobscript_plot_obs.sh
+++ jobscript_plot_obs.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
 #PBS -S /bin/bash
 #PBS -l walltime=0:59:00
-#PBS -l select=1:ncpus=8:ompthreads=8:mem=24GB
+#PBS -l select=1:ncpus=1:ompthreads=1:mem=24GB
 #PBS -j oe
 #PBS -W umask=022
-#PBS -N pysteps_run
-
-# Run this script as follows:
-# qsub -vtimestamp=202308251600 jobscript_plot.sh 
-
+#PBS -N plot_obs 
 echo -----------------------------------
 echo START : `date -u +"%Y%m%d %H:%M:%S"`
 echo -----------------------------------
 
+# Run this script as follows:
+# qsub -vtimestamp=YYYYMMDDHHMM,nfiles=24 jobscript_plot_obs.sh
+
 #set -x # echo script lines as they are executed
 #set -e # stop the shell on first error
 #set -u # fail when using an undefined variable
-source /home/ledecruz/.bashrc
-export OMP_NUM_THREADS=8
-workdir=/home/ledecruz/projects/rmi-scripts
-script=run_steps.py
-#script=plot_nwc.py
+
+source $HOME/.bashrc
+export OMP_NUM_THREADS=1
+
+workdir=$HOME/projects/rmi-scripts
+logdir=${workdir}/data/log
+logfile=${logdir}/obsplot_${timestamp}
+script=plot_obs.py
 conda init
 conda activate pysteps_dev
+cd $workdir
+mkdir -p $logdir
 
 set -x
-cd $workdir
-python ${script} $timestamp 72 8 8 > $workdir/data/log/steps_$timestamp.log 2> $workdir/data/log/steps_$timestamp.err
+python ${script} ${timestamp} ${nfiles} > ${logfile}.log 2> ${logfile}.err
 set +x
 
 echo -----------------------------------
